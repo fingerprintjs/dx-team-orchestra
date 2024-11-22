1a06a65e15427b5e77286db5eceadb04ebb77f17
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2024-11-22 18:25:38 +0100

    feat: add 1 method to node SDK

---
+++ README.md
@@ -0,0 +1,9 @@
+# DX Team Orchestra
+
+More info: https://www.notion.so/fingerprintjs/E2E-tests-for-server-SDKs-14302f125ebd80ceadfcf9eafeea1d41
+
+Work in progress.
+
+## How to run
+To run node SDK: just run `pnpm start` in project root.
+Then You can make GET request in format: `http://localhost:3002/getEvents?apiKey=<SECRET_KEY>&region=<REGION>&requestId=<REQUEST_ID>`
--- projects/node-sdk/src/index.ts
+++ projects/node-sdk/src/index.ts
@@ -1,1 +1,56 @@
-console.log('Hello World');
+import express, { Request, Response } from 'express'
+
+import {
+    FingerprintJsServerApiClient, Region, RequestError,
+} from '@fingerprintjs/fingerprintjs-pro-server-api'
+
+const app = express()
+
+const port = 3002
+
+function getRegion(region: string): Region {
+    switch (region) {
+        case 'eu':
+            return Region.EU
+        case 'ap':
+            return Region.AP
+        default:
+            return Region.Global
+    }
+}
+
+app.get('/getEvents', async (req: Request, res: Response) => {
+    const {apiKey = '', region = '', requestId = ''} = req.query;
+    const client = new FingerprintJsServerApiClient({
+        apiKey: apiKey.toString(),
+        region: getRegion(region.toString()),
+    })
+    try {
+        const event = await client.getEvent(requestId.toString())
+        res.send({
+            code: 200, // or 4**, 5** in case of error
+            originalResponse: event,
+            parsedResponse: event,
+        })
+    } catch (error) {
+        if (error instanceof RequestError) {
+            const originalResponse = await error.response.text()
+            res.send({
+                code: error.statusCode, // or 4**, 5** in case of error
+                originalResponse: originalResponse,
+                parsedResponse: error.responseBody,
+            })
+        } else {
+            res.send({
+                code: 500, // or 4**, 5** in case of error
+                originalResponse: error,
+                parsedResponse: error,
+            })
+        }
+    }
+
+})
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
