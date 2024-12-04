ebccfd9f852b0e8bf4ddc76f0c64624749fa39fe
MariamMahran <mariam.mahran@fingerprint.com>
2024-12-04 12:20:08 +0300

    automate generating request ID

--- projects/conductor/testData.ts
+++ projects/conductor/testData.ts
@@ -2,10 +2,12 @@ const testData = {
   valid: {
     apiKey: "ftxPJdxnMlPIeZV09RoT",
     region: "Global",
-    requestId: "1732537613156.sEoTOe",
   },
   config: {
     baseURL: "http://localhost:3002",
+  },
+  generatidentification: {
+    publicApiKey: "rO6UOgRbMculDuXmJl4g",
   },
 };
 export default testData;
