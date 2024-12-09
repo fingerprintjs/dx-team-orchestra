3799fe53ffbf2f1294c4a3cd88eae8e2ba696140
MariamMahran <mariam.mahran@fingerprint.com>
2024-12-09 15:05:10 +0300

    update methods to catch json errors

--- projects/node-sdk/src/handlers/deleteVisitorData.ts
+++ projects/node-sdk/src/handlers/deleteVisitorData.ts
@@ -1,31 +1,30 @@
-import { Handler, MusicianResponse } from '../types';
-import {
-    FingerprintJsServerApiClient,
-} from '@fingerprintjs/fingerprintjs-pro-server-api';
-import { getRegion, unwrapError } from '../utils';
+import { Handler, MusicianResponse } from "../types";
+import { FingerprintJsServerApiClient } from "@fingerprintjs/fingerprintjs-pro-server-api";
+import { getRegion, unwrapError } from "../utils";
 
 interface QueryParams {
-    apiKey?: string;
-    region?: string;
-    visitorId?: string;
+  apiKey?: string;
+  region?: string;
+  visitorId?: string;
 }
 
 export const deleteVisitorData: Handler<QueryParams> = async (req, res) => {
-    const {apiKey = '', region = '', visitorId = ''} = req.query;
+  const { apiKey = "", region = "", visitorId = "" } = req.query;
+  let result: MusicianResponse<void>;
+  try {
     const client = new FingerprintJsServerApiClient({
-        apiKey: apiKey,
-        region: getRegion(region),
+      apiKey: apiKey,
+      region: getRegion(region),
     });
-    let result: MusicianResponse<void>;
-    try {
-        const event = await client.deleteVisitorData(visitorId);
-        result = {
-            code: 200,
-            originalResponse: event,
-            parsedResponse: event,
-        };
-    } catch (error) {
-        result = await unwrapError<void>(error);
-    }
-    res.send(result);
+
+    const event = await client.deleteVisitorData(visitorId);
+    result = {
+      code: 200,
+      originalResponse: event,
+      parsedResponse: event,
+    };
+  } catch (error) {
+    result = await unwrapError<void>(error);
+  }
+  res.send(result);
 };
