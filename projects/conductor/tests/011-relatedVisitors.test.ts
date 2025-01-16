bae7f70c0f99237fd4e18d18dfd50a62a78d2808
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-01-16 16:35:25 +0100

    chore: change visitor ID

--- projects/conductor/tests/011-relatedVisitors.test.ts
+++ projects/conductor/tests/011-relatedVisitors.test.ts
@@ -1,11 +1,16 @@
 import {expect} from "@playwright/test";
 import testData from "../utils/testData";
 import {test} from "../utils/playwright";
+import {identify} from "../htmlScripts/runIdentification";
 
 test.describe('Related Visitors Suite', () => {
   test('with valid apiKey and visitor id', async ({sdkApi}) => {
+    const {visitorId} = await identify({
+      publicApiKey: testData.credentials.maxFeaturesUS.publicKey,
+    })
+
     const {data} = await sdkApi.getRelatedVisitors({
-      visitorId: 'SzJDU0OIz1AoClm2vtcH',
+      visitorId,
       apiKey: testData.credentials.maxFeaturesUS.privateKey,
     })
 
