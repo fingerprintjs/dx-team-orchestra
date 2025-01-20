7720d22a15da95619131c68dcbdb132a91b1c081
Przemysław Żydek <przemyslawzydek@gmail.com>
2025-01-20 11:48:43 +0100

    feat: add more related visitors tests

--- projects/conductor/tests/011-relatedVisitors.test.ts
+++ projects/conductor/tests/011-relatedVisitors.test.ts
@@ -18,4 +18,80 @@ test.describe('Related Visitors Suite', () => {
       relatedVisitors: []
     })
   })
+
+  test('with invalid region', async ({sdkApi}) => {
+    const {visitorId} = await identify({
+      publicApiKey: testData.credentials.maxFeaturesUS.publicKey,
+    })
+
+    const {data, response} = await sdkApi.getRelatedVisitors({
+      visitorId,
+      apiKey: testData.credentials.maxFeaturesUS.privateKey,
+      region: 'eu',
+    })
+
+    expect(response.status()).toEqual(403)
+
+    expect(data).toStrictEqual({
+      error: {
+        code: 'WrongRegion',
+        message: 'wrong region'
+      }
+    })
+  })
+
+  test('with invalid visitor id', async ({sdkApi}) => {
+    const {data, response} = await sdkApi.getRelatedVisitors({
+      visitorId: testData.mocks.invalid.visitorId,
+      apiKey: testData.credentials.maxFeaturesUS.privateKey,
+    })
+
+    expect(response.status()).toEqual(404)
+
+    expect(data).toStrictEqual({
+      error: {
+        code: 'VisitorNotFound',
+        message: 'visitor not found'
+      }
+    })
+  })
+
+  test('with api key that does not have the feature enabled', async ({sdkApi}) => {
+    const {visitorId} = await identify({
+      publicApiKey: testData.credentials.minFeaturesUS.publicKey,
+    })
+
+
+    const {data, response} = await sdkApi.getRelatedVisitors({
+      visitorId,
+      apiKey: testData.credentials.minFeaturesUS.privateKey,
+    })
+
+    expect(response.status()).toEqual(403)
+    expect(data).toStrictEqual({
+      error: {
+        code: 'FeatureNotEnabled',
+        message: 'feature not enabled'
+      }
+    })
+  })
+
+  test('with invalid api key', async ({sdkApi}) => {
+    const {visitorId} = await identify({
+      publicApiKey: testData.credentials.maxFeaturesUS.publicKey,
+    })
+
+    const {data, response} = await sdkApi.getRelatedVisitors({
+      visitorId,
+      apiKey: testData.credentials.invalid.privateKey,
+    })
+
+    expect(response.status()).toEqual(403)
+    expect(data).toStrictEqual({
+      error: {
+        code: 'TokenNotFound',
+        message: 'secret key is not found'
+      }
+    })
+  })
 })
