d875971175d33be376524358306f768234edc91a
Orkun <orkuncakilkaya@gmail.com>
2025-09-04 12:45:31 +0300

    fix: java sdk eventscontroller error

--- projects/java-sdk/src/main/java/com/example/java_sdk/EventsController.java
+++ projects/java-sdk/src/main/java/com/example/java_sdk/EventsController.java
@@ -15,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 import java.util.Map;
 
