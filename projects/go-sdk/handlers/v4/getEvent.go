355c0f37a80764959254aebac04612a0f71ea38a
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-03-11 16:51:20 +0100

    feat(go-sdk): support `ruleset_id` query parameter in `GetEvent` handler

--- projects/go-sdk/handlers/v4/getEvent.go
+++ projects/go-sdk/handlers/v4/getEvent.go
@@ -4,23 +4,33 @@ import (
 	"encoding/json"
 	"go-sdk/fingerprintv4"
 	"net/http"
+
+	fingerprint "github.com/fingerprintjs/go-sdk/v8"
 )
 
 type getEventsQueryParams struct {
-	ApiKey  string `json:"api_key"`
-	Region  string `json:"region"`
-	EventID string `json:"event_id"`
+	ApiKey    string `json:"api_key"`
+	Region    string `json:"region"`
+	EventID   string `json:"event_id"`
+	RulesetID string `json:"ruleset_id"`
 }
 
 func GetEvent(w http.ResponseWriter, r *http.Request) {
 	queryParams := getEventsQueryParams{
-		ApiKey:  r.URL.Query().Get("api_key"),
-		Region:  r.URL.Query().Get("region"),
-		EventID: r.URL.Query().Get("event_id"),
+		ApiKey:    r.URL.Query().Get("api_key"),
+		Region:    r.URL.Query().Get("region"),
+		EventID:   r.URL.Query().Get("event_id"),
+		RulesetID: r.URL.Query().Get("ruleset_id"),
 	}
 
 	client := fingerprintv4.Init(queryParams.ApiKey, queryParams.Region)
-	response := fingerprintv4.ProcessResponse(client.GetEvent(r.Context(), queryParams.EventID))
+
+	var options []fingerprint.GetEventOption
+	if queryParams.RulesetID != "" {
+		options = append(options, fingerprint.WithRulesetID(queryParams.RulesetID))
+	}
+
+	response := fingerprintv4.ProcessResponse(client.GetEvent(r.Context(), queryParams.EventID, options...))
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(200)
