61c0efe63cd0cd53f68e7540721aa5a1564a968c
Dan McNulty <daniel.mcnulty@fingerprint.com>
2026-09-25 11:47:06 -0500

    chore: upgrade java-sdk

--- projects/java-sdk/build.gradle.kts
+++ projects/java-sdk/build.gradle.kts
@@ -26,8 +26,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	implementation("com.github.fingerprintjs:fingerprint-pro-server-api-java-sdk:v7.13.0")
-	implementation("com.github.fingerprintjs:java-sdk:v8.7.0")
+	implementation("com.github.fingerprintjs:fingerprint-pro-server-api-java-sdk:v7.14.1")
+	implementation("com.github.fingerprintjs:java-sdk:v8.7.1")
 }
 
 tasks.withType<Test> {
